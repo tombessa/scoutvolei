2b987af5e09518fc5d368bd5862bf0b8d4423c1e
Antonyonne Soares Bessa <tombessa@gmail.com>
2026-09-15 12:08:34 -0300

    chore: renovar cache offline para novo indicador

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "scout-volei-trt-v1";
+const CACHE_NAME = "scout-volei-trt-v2";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
